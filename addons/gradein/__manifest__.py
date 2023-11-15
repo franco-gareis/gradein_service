9d7839657ab6bcb5d13e97c10602db0eb2b6e2be
Franco Gareis <franco.gareis@mirgor.com>
2023-11-15 17:07:15 -0300

    Modify docker-compose, add manifest for odoo

---
+++ addons/gradein/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Gradein",
+    "version": "1.0",
+    "category": "",
+    "summary": "",
+    "author": ["Ariel Montenegro", "Yamil Ferrufino", "Franco Gareis"],
+    "website": "https://www.callefalsa.com.ar",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+    "description": """
+    """,
+}
