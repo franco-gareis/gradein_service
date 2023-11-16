77cfdd8d1f3d50e88aa1156baa88b763e90f6ad1
Ariel Montenegro <ariel.montenegro@mirgor.com>
2023-11-16 14:49:40 -0300

    Translate strings from fields

--- addons/gradein/models/answer_model.py
+++ addons/gradein/models/answer_model.py
@@ -12,8 +12,8 @@ class AnswerModel(models.Model):
     _name = "gradein.answer"
     _description = "Answer Model"
 
-    name = fields.Char(required=True, help="Name Answer", string="Name")
-    currency = fields.Many2one("res.currency", string="Currency")
-    price_reduction = fields.Monetary(string="Price Reduction", currency_field="currency")
-    blocking = fields.Boolean(help="Cannot continue with the form", default=False)
+    name = fields.Char(required=True, help="Name Answer", string="Nombre")
+    currency = fields.Many2one("res.currency", string="Moneda")
+    price_reduction = fields.Monetary(string="Reduce el precio en", currency_field="currency")
+    blocking = fields.Boolean(help="Cannot continue with the form", default=False, string="Respuesta bloqueante")
     activo = fields.Boolean(help="Activate o desactivate", default=True)
