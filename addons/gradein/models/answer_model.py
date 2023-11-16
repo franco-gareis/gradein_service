18e19be36974009a7f38ca12ee179a966d9bdbc0
Ariel Montenegro <ariel.montenegro@mirgor.com>
2023-11-16 14:11:58 -0300

    Model answer is added

---
+++ addons/gradein/models/answer_model.py
@@ -0,0 +1,19 @@
+from odoo import fields, models
+
+
+class AnswerModel(models.Model):
+    """Questions for gradein form, 
+    if you select "blocking" you 
+    cannot continue the form
+
+    Args:
+        models (_type_): Model
+    """
+    _name = "answer.model"
+    _description = "Answer Model"
+
+    name = fields.Char(required=True, help="Name Answer", string="Name")
+    currency = fields.Many2one("res.currency", string="Currency")
+    price_reduction = fields.Monetary(string="Price Reduction", currency_field="currency")
+    blocking = fields.Boolean(help="Cannot continue with the form", default=False)
+    activo = fields.Boolean(help="Activate o desactivate", required=True, default=True)
