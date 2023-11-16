7abdbf5de0c85e788bc278ab9f198df53c49f7b8
Yamil <yamil.ferrufino@mirgor.com>
2023-11-16 14:59:16 -0300

    feat: add configuration menu

--- addons/gradein/models/question_model.py
+++ addons/gradein/models/question_model.py
@@ -2,14 +2,14 @@
 from odoo import models, fields, api
 
 
-class QuestionModel (models.Model):
+class QuestionModel(models.Model):
     
     _name = 'gradein.question'
     _description = 'gradein questions'
     
     name = fields.Char(string="Nombre")
-    active = fields.Boolean(string="activa")
-    equipment_type_ids = fields.Many2one(comodel_name="gradein.equipment_types",string="Tipos de equipo")
+    active = fields.Boolean(string="activa",default=True)
+    equipment_type_ids = fields.Many2one(comodel_name="gradein.equipment_type",string="Tipos de equipo")
     answer_ids = fields.One2many(comodel_name="gradein.answer",string="Respuestas posibles")
     
 
