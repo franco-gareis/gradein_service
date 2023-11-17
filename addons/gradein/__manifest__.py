c05984aeba6550553361d034cecb07188a4d4540
Yamil <yamil.ferrufino@mirgor.com>
2023-11-17 11:44:34 -0300

    feat: delete pointers HEAD

--- addons/gradein/__manifest__.py
+++ addons/gradein/__manifest__.py
@@ -13,11 +13,9 @@
         'views/services_menu.xml',
         'views/answer_model.xml',
         'views/equipment_type.xml',
-<<<<<<< HEAD
         'views/question.xml'
-=======
         'views/equipment.xml',
->>>>>>> e3acacbeb6ae141220d6e015e8fd9d88ee1d0090
+
     ],
     "installable": True,
     "auto_install": False,
