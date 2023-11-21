02e53f88fc23d879cc473d1b37be216242adde92
Franco Gareis <franco.gareis@mirgor.com>
2023-11-21 16:07:23 -0300

    Ensure that record exists, else return not_found

--- addons/gradein/controllers/gradein_controller.py
+++ addons/gradein/controllers/gradein_controller.py
@@ -9,23 +9,26 @@ HEADERS = [("Content-Type", "application/json"), ("Cache-Control", "no-store")]
 class GradeInController(http.Controller):
 
     @route(
-        "/gradein/questions/equipment_type_id/<int:equipment_type_id>",
+        "/gradein/questions/equipment-type-id/<int:equipment_type_id>",
         type="http",
         auth="public",
         methods=["GET"],
-        cors="*",
     )
     def get_gradein_questions(self, equipment_type_id):
         equipment = request.env["gradein.equipment.type"].sudo().browse([equipment_type_id])
-        data = json.dumps(
-            {
-                "equipment_type_id": equipment.id,
-                "equipment_type_name": equipment.name,
-                "questions": [
-                    {question.name: question.answer_ids.mapped("name")}
-                    for question in equipment.question_ids
-                ],
-            }
-        )
 
-        return request.make_response(data, headers=HEADERS)
+        if equipment.exists():
+            data = json.dumps(
+                {
+                    "equipment_type_id": equipment.id,
+                    "equipment_type_name": equipment.name,
+                    "questions": [
+                        {question.name: question.answer_ids.mapped("name")}
+                        for question in equipment.question_ids
+                    ],
+                }
+            )
+
+            return request.make_response(data, headers=HEADERS)
+        else:
+            return request.not_found()
