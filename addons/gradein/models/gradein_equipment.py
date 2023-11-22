cf5762866ffc5062b3ad2f319052f126e488530f
Franco Gareis <franco.gareis@mirgor.com>
2023-11-22 17:37:37 -0300

    Improve changes given by Ale :)

--- addons/gradein/models/gradein_equipment.py
+++ addons/gradein/models/gradein_equipment.py
@@ -1,17 +1,41 @@
 from odoo import fields, models, api
 from odoo.exceptions import ValidationError
 
-class GradeInEquipment(models.Model):
-    _name = 'gradein.equipment'
-    _description = "Equipment Model"
-    _rec_name = 'name'
 
-    name = fields.Char(string='Nombre', help='Name of the equipment', required=True)
-    active = fields.Boolean(string='Activo', help='If the equipment is active')
-    image = fields.Image(string='Imagen', verify_resolution=True, help='Image of the equipment')
-    price = fields.Monetary(string='Precio', currency_field='currency_id', help='Price of the equipment')
-    description = fields.Text(string='Descripcion', help='Resume of the equipment')
-    currency_id = fields.Many2one('res.currency', default=lambda x: x.env.company.currency_id, string='Moneda')
+class GradeInEquipment(models.Model):
+    _name = "gradein.equipment"
+    _description = "Equipment Model"
+    _rec_name = "name"
+
+    name = fields.Char(string="Nombre", help="Name of the equipment", required=True)
+    active = fields.Boolean(
+        string="Activo",
+        required=True,
+        default=True,
+        help="If the equipment is active",
+    )
+    image = fields.Image(
+        string="Imagen",
+        verify_resolution=True,
+        help="Image of the equipment",
+        required=True,
+    )
+    price = fields.Monetary(
+        string="Precio",
+        currency_field="currency_id",
+        help="Price of the equipment",
+        required=True,
+    )
+    description = fields.Text(
+        string="Descripcion", help="Resume of the equipment", required=True
+    )
+    currency_id = fields.Many2one(
+        "res.currency",
+        default=lambda x: x.env.company.currency_id,
+        string="Moneda",
+        required=True,
+    )
+
 
     @api.constrains('price')
     def _check_price_not_zero(self):
