9bd1e23042dedd26d21340a21de616847b115bb5
Yamil <yamil.ferrufino@mirgor.com>
2023-11-22 13:51:06 -0300

    feat: add modifies to gradein_reject_reason.py

--- addons/gradein/models/gradein_reject_reason.py
+++ addons/gradein/models/gradein_reject_reason.py
@@ -3,8 +3,8 @@ from odoo import models, fields, api
 
 class GradeInRejectReason(models.Model):
     
-    _name = 'gradein.reject.reason'
-    _description = 'gradein reject reason'
+    _name = "gradein.reject.reason"
+    _description = "gradein reject reason"
     
-    name = fields.Char(string="Nombre")
+    name = fields.Char(string="Nombre",required=True)
     active = fields.Boolean(string="activa",default=True)
