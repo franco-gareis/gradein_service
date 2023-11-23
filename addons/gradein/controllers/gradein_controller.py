c7d890c16bebfa12db77b33962b0958c09fa9a28
Franco Gareis <franco.gareis@mirgor.com>
2023-11-23 11:05:45 -0300

    Improve changes

--- addons/gradein/controllers/gradein_controller.py
+++ addons/gradein/controllers/gradein_controller.py
@@ -9,20 +9,7 @@ HEADERS = [("Content-Type", "application/json"), ("Cache-Control", "no-store")]
 
 
 class GradeInController(http.Controller):
-    def _process_data_by_equipment_id(self, recordset):
-        data = json.dumps(
-            {
-                "equipment_type_id": recordset.id,
-                "equipment_type_name": recordset.name,
-                "questions": [
-                    {question.name: question.answer_ids.mapped("name")}
-                    for question in recordset.question_ids
-                ],
-            }
-        )
-        return data
-
-    def _process_all_questions(self, recordset):
+    def _process_data(self, recordset):
         data = json.dumps(
             [
                 {
@@ -43,18 +30,14 @@ class GradeInController(http.Controller):
 
         equipment_type_id = request.params.get("equipment_type_id")
 
-        try:
-            if equipment_type_id:
-                recordset = (
-                    request.env["gradein.equipment.type"]
-                    .sudo()
-                    .browse([int(equipment_type_id)])
-                )
-                data = self._process_data_by_equipment_id(recordset)
-            else:
-                recordset = request.env["gradein.equipment.type"].sudo().search([])
-                data = self._process_all_questions(recordset)
+        if equipment_type_id:
+            recordset = request.env["gradein.equipment.type"].sudo().browse([int(equipment_type_id)])
+        else:
+            recordset = request.env["gradein.equipment.type"].sudo().search([])
 
-            return request.make_response(data, headers=HEADERS)
+        try:
+            data = self._process_data(recordset)
         except MissingError:
             return request.not_found()
+
+        return request.make_response(data, headers=HEADERS)
