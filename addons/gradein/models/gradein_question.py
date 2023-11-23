c14ca0124bf8fadab4044e036910d7bef7fc1eaa
Yamil <yamil.ferrufino@mirgor.com>
2023-11-23 15:19:46 -0300

    feat:add required field to GradeinQuestion

--- addons/gradein/models/gradein_question.py
+++ addons/gradein/models/gradein_question.py
@@ -7,9 +7,9 @@ class GradeInQuestion(models.Model):
     _name = 'gradein.question'
     _description = 'gradein questions'
     
-    name = fields.Char(string="Nombre")
+    name = fields.Char(string="Nombre",required=True)
     active = fields.Boolean(string="activa",default=True)
-    equipment_type_ids = fields.Many2many(comodel_name="gradein.equipment.type",string="Tipos de equipo")
-    answer_ids = fields.One2many(comodel_name="gradein.answer",string="Respuestas posibles", inverse_name="question_id")
+    equipment_type_ids = fields.Many2many(comodel_name="gradein.equipment.type",string="Tipos de equipo",required=True)
+    answer_ids = fields.One2many(comodel_name="gradein.answer",string="Respuestas posibles", inverse_name="question_id",required=True)
     
 
