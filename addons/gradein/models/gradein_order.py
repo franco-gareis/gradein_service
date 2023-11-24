f325f825b08bbc8331161e6bf66851a96af9b260
Franco Gareis <franco.gareis@mirgor.com>
2023-11-24 12:28:30 -0300

    Add relation between gradein.order Model & gradein.question.answer Model

--- addons/gradein/models/gradein_order.py
+++ addons/gradein/models/gradein_order.py
@@ -13,24 +13,28 @@ class GradeInOrder(models.Model):
         string="Estado de la orden",
     )
     equipment_id = fields.Many2one(
-        comodel_name="gradein.equipment", string="Equipo", help="Equipment of the order"
-    )
-    imei = fields.Char(
-        required=True, string="IMEI", help="IMEI of the equipment to check"
-    )
-    image_ids = fields.Many2one(
         comodel_name="gradein.equipment",
+        string="Equipo",
+        help="Equipment of the order"
+    )
+    image_ids = fields.Many2many(
+        comodel_name="gradein.images",
         string="Imagenes",
         help="Images the of the equipment",
     )
     review = fields.Text(
-        string="Resumen de la evaluacion", help="Short review of the evaluation"
+        string="Resumen de la evaluacion",
+        help="Short review of the evaluation"
     )
     reject_motive_id = fields.Many2one(
-        comodel_name="gradein.reject.reason", string="Motivo de rechazo"
+        comodel_name="gradein.reject.reason",
+        string="Motivo de rechazo"
     )
+    imei = fields.Char(string="IMEI", help="IMEI of the equipment to check")
     partner_id = fields.Many2one(comodel_name="res.partner", string="Cliente")
     price = fields.Float(string="Importe a pagar", help="Price that client will pay")
-    answer_ids = fields.Many2many(comodel_name="gradein.answer", string="Respuestas")
-    question_id = fields.Many2one(comodel_name="gradein.question", string="Pregunta")
-    answer_id = fields.Many2one(comodel_name="gradein.answer", string="Respuesta")
+    question_answer_id = fields.One2many(
+        comodel_name="gradein.question.answer",
+        inverse_name="order_id",
+        string="Respuestas"
+    )
