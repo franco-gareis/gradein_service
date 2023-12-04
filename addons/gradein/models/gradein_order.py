4e2a073f6bc95af44dbac51faacc7addb11277fc
Yamil <yamil.ferrufino@mirgor.com>
2023-12-04 14:51:32 -0300

    quit constrain

--- addons/gradein/models/gradein_order.py
+++ addons/gradein/models/gradein_order.py
@@ -143,38 +143,41 @@ class GradeInOrder(models.Model):
     
     @api.constrains("imei")
     def validate_imei(self):
-        url = f"https://mirgor-alkemy-imei-api.azurewebsites.net/api/check_imei/{self.imei}"
+        
+        if self.equipment_type_name == "smartphone":
+            
+            url = f"https://mirgor-alkemy-imei-api.azurewebsites.net/api/check_imei/{self.imei}"
 
-        try:
-            response = requests.get(url, timeout=10)
-            response.raise_for_status()
+            try:
+                response = requests.get(url, timeout=10)
+                response.raise_for_status()
 
-        except requests.exceptions.HTTPError as errh:
-            if response.status_code == 404:
-                raise ValidationError(f"La URL: {url} no es valida  ") from errh
+            except requests.exceptions.HTTPError as errh:
+                if response.status_code == 404:
+                    raise ValidationError(f"La URL: {url} no es valida  ") from errh
 
-        except requests.exceptions.ConnectionError as conerr:
-            raise ValidationError(
-                "Hubo un error al iniciar la conexion al servidor"
-            ) from conerr
+            except requests.exceptions.ConnectionError as conerr:
+                raise ValidationError(
+                    "Hubo un error al iniciar la conexion al servidor"
+                ) from conerr
 
-        response_dict = response.json()
-        is_valid_imei = response_dict.get("valid")
+            response_dict = response.json()
+            is_valid_imei = response_dict.get("valid")
 
-        if not is_valid_imei:
-            raise ValidationError("El imei no es valido para realizar esta operacion")
+            if not is_valid_imei:
+                raise ValidationError("El imei no es valido para realizar esta operacion")
 
-        else:
-            notification = {
-                "type": "ir.actions.client",
-                "tag": "display_notification",
-                "params": {
-                    "title": ("Validador de IMEI"),
-                    "message": "El IMEI ingresado es valido",
-                    "type": "success",  # types: success,warning,danger,info
-                    "sticky": True,  # True/False will display for few seconds if false
-                },
-            }
+            else:
+                notification = {
+                    "type": "ir.actions.client",
+                    "tag": "display_notification",
+                    "params": {
+                        "title": ("Validador de IMEI"),
+                        "message": "El IMEI ingresado es valido",
+                        "type": "success",  # types: success,warning,danger,info
+                        "sticky": True,  # True/False will display for few seconds if false
+                    },
+                }
 
             return notification
 
