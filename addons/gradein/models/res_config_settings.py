b91a1f6264ec76366f1ef47b8ff1edc7456a2109
lucbece-mirgor <lucas.becerra@mirgor.com>
2023-12-12 10:37:01 -0300

    fix equipment_limit_per_month constrains

--- addons/gradein/models/res_config_settings.py
+++ addons/gradein/models/res_config_settings.py
@@ -13,7 +13,7 @@ class GradeinConfigSettings(models.TransientModel):
 
     @api.constrains("equipment_limit_per_month")
     def _check_equipment_limit(self):
-        if self.equipment_limit_per_month < 0 :
+        if self.equipment_limit_per_month <= 0 :
             raise ValidationError(
                 "El límite de equipos por mes debe ser un número positivo."
             )
