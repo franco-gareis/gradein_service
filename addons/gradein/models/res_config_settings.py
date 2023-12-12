76faa4a09fbb5872875d5c383fcc84ff5127f9ad
Lucas Becerra <60028580+lucbece@users.noreply.github.com>
2023-12-12 10:47:34 -0300

    Add res_config_settings model to handle module settings
    Co-authored-by: lucbece-mirgor <lucas.becerra@mirgor.com>

---
+++ addons/gradein/models/res_config_settings.py
@@ -0,0 +1,19 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class GradeinConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    equipment_limit_per_month = fields.Integer(
+        string="Límite de Equipos por Mes",
+        config_parameter="gradein.equipment_limit_per_month",
+        default=2,
+    )
+
+    @api.constrains("equipment_limit_per_month")
+    def _check_equipment_limit(self):
+        if self.equipment_limit_per_month <= 0 :
+            raise ValidationError(
+                "El límite de equipos por mes debe ser un número positivo."
+            )
