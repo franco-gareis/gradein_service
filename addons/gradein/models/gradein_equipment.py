05b44f37f0472e95f305489d125d3932ac01ff33
pedro-matias-ghiglione <149110440+pedro-matias-ghiglione@users.noreply.github.com>
2023-12-12 11:48:57 -0300

    ODOO-110/Add_log_in_equipment
    * ODOO-110/Add_log_in_equipment
    
    * ODOO-119/Add_log_in_equipment (currency not tracking)

--- addons/gradein/models/gradein_equipment.py
+++ addons/gradein/models/gradein_equipment.py
@@ -6,34 +6,38 @@ class GradeInEquipment(models.Model):
     _name = "gradein.equipment"
     _description = "Equipment Model"
     _rec_name = "name"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
 
-    name = fields.Char(string="Nombre", help="Name of the equipment", required=True)
+    name = fields.Char(
+        string="Nombre", help="Name of the equipment", required=True, tracking=True
+    )
     active = fields.Boolean(
         string="Activo",
         required=True,
         default=True,
         help="If the equipment is active",
+        tracking=True,
     )
     image = fields.Image(
         string="Imagen",
         verify_resolution=True,
         help="Image of the equipment",
-        required=True,
+        tracking=True,
     )
     price = fields.Monetary(
         string="Precio",
         currency_field="currency_id",
         help="Price of the equipment",
         required=True,
+        tracking=True,
     )
     equipment_type_id = fields.Many2one(
         comodel_name="gradein.equipment.type",
         string="Tipo de equipo",
-        help="Wich type is the equipment"
+        help="Wich type is the equipment",
+        tracking=True,
     )
-    description = fields.Text(
-        string="Descripcion", help="Resume of the equipment", required=True
-    )
+    description = fields.Text(string="Descripcion", help="Resume of the equipment")
     currency_id = fields.Many2one(
         "res.currency",
         default=lambda x: x.env.company.currency_id,
@@ -41,11 +45,10 @@ class GradeInEquipment(models.Model):
         required=True,
     )
 
-
-    @api.constrains('price')
+    @api.constrains("price")
     def _check_price_not_zero(self):
         """Method validator for records so price is not zero"""
 
         for record in self:
             if record.price <= 0:
-                raise ValidationError('El precio no puede ser menor o igual a 0')
+                raise ValidationError("El precio no puede ser menor o igual a 0")
